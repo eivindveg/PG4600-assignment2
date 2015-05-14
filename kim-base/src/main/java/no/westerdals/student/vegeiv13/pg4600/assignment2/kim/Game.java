60c1d561bc5db0b6cbcdd60738993f9a521a84c3
eivindveg <eivindveg@hotmail.com>
2015-05-14 15:41:24 +0200

    add basic kim's game setup

--- kim-base/src/main/java/no/westerdals/student/vegeiv13/pg4600/assignment2/kim/Game.java
+++ kim-base/src/main/java/no/westerdals/student/vegeiv13/pg4600/assignment2/kim/Game.java
@@ -1,4 +1,23 @@
 package no.westerdals.student.vegeiv13.pg4600.assignment2.kim;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+
+    private final Difficulty difficulty;
+    private List<Round> rounds;
+
+    public Game(final int rounds, final Difficulty difficulty, List<String> availableWords) {
+        this.rounds = new ArrayList<>();
+        this.difficulty = difficulty;
+        generateRounds(rounds, availableWords);
+    }
+
+    private void generateRounds(final int rounds, final List<String> availableWords) {
+        final int numWords = difficulty.getWords();
+        for(int i = 0; i < rounds; i++) {
+            this.rounds.add(new Round(availableWords, numWords));
+        }
+    }
 }
